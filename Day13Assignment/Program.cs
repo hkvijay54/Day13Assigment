0b77161384822c3bea9677ed1daa8bb9a04a1e88
hkvijay54 <hkvijay54@gmail.com>
2022-01-06 13:03:48 +0530

    uc-2 program

--- Day13Assignment/Program.cs
+++ Day13Assignment/Program.cs
@@ -11,6 +11,10 @@ namespace Day13Assignment
             Console.WriteLine("FIND THE MAXIMUM INTEGER NUMBER!");
             int output = UC1FIND_MAXIMUM_NUMBER.MaximumIntegerNumber(11, 5, 40);
             Console.WriteLine(output);
+
+            Console.WriteLine("FIND THE MAXIMUM Float NUMBER!");
+            float floatoutput = (float)UC2_USEFLOAT_FIND_MAX_NUMBER.MaximumFloatNumber(2.3, 4.5, 6.6);
+            Console.WriteLine(floatoutput);
         }
     }
 }
